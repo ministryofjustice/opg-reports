8084acb2d21572aae39b4c345f85ea10e1203650
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-06-25 13:04:01 +0100

    run the server, setup static file handling

---
+++ services/front/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"embed"
+	"log/slog"
+	"net/http"
+	"opg-reports/services/front/cnf"
+	"opg-reports/services/front/server"
+	"opg-reports/services/front/tmpl"
+	"opg-reports/shared/files"
+	"opg-reports/shared/logger"
+)
+
+//go:embed templates
+var templateFs embed.FS
+
+//go:embed config.json
+var configContent []byte
+
+func main() {
+
+	logger.LogSetup()
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	templateFS := files.NewFS(templateFs, "templates")
+	templateFiles := tmpl.Files(templateFS, "")
+
+	conf, _ := cnf.Load(configContent)
+	serve := server.New(conf, templateFiles)
+	serve.Register(mux)
+
+	addr := ":8080" // env.Get("FRONT_ADDR", ":8080")
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	slog.Info("starting web server",
+		slog.String("log_level", logger.Level().String()),
+		slog.String("address", addr),
+	)
+	server.ListenAndServe()
+}
