7651c2bea45492c7e2a2028fe6d01b87b8ecdaa3
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-07-11 15:00:10 +0100

    disable pinned

--- terraform/account/workspace_manager.tf
+++ terraform/account/workspace_manager.tf
@@ -1,4 +1,4 @@
 module "workspace_manager" {
-  source  = "git@github.com:ministryofjustice/opg-terraform-workspace-manager.git//terraform/workspace_cleanup?ref=0.3.2"
+  source  = "git@github.com:ministryofjustice/opg-terraform-workspace-manager.git//terraform/workspace_cleanup"
   enabled = local.environment.account_name == "development" ? true : false
 }
