9e3ad5ff7eb4139baba38ccf764934071f0d18cb
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-07-11 15:11:18 +0100

    inline workspace manager

--- terraform/account/workspace_manager.tf
+++ terraform/account/workspace_manager.tf
@@ -1,4 +1,21 @@
-module "workspace_manager" {
-  source  = "git@github.com:ministryofjustice/opg-terraform-workspace-manager.git//terraform/workspace_cleanup"
-  enabled = local.environment.account_name == "development" ? true : false
+
+resource "aws_dynamodb_table" "workspace_cleanup_table" {
+  count        = local.environment.account_name == "development" ? 1 : 0
+  name         = "WorkspaceCleanup"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "WorkspaceName"
+
+  attribute {
+    name = "WorkspaceName"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ExpiresTTL"
+    enabled        = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
