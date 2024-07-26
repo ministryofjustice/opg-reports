365f1edc6aaad963e1eba5917a9236053c025cdb
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-07-26 09:33:07 +0100

    try an oidc role for s3 access

--- terraform/environment/s3.tf
+++ terraform/environment/s3.tf
@@ -36,3 +36,43 @@ data "aws_iam_policy_document" "allow_data_role_access" {
     ]
   }
 }
+
+# OIDC access to the s3 bucket
+module "reports_oidc_role_gha_ecr_push" {
+  source      = "git@github.com:ministryofjustice/opg-terraform-aws-account//modules/github_oidc_roles?ref=v7.4.4"
+  name        = "opg-reports-github-actions-s3"
+  description = "A role for S3 permissions for GitHub Actions"
+
+  permissions = [
+    "repo:ministryofjustice/opg-reports:pull_request",
+    "repo:ministryofjustice/opg-reports:ref:refs/heads/main",
+    "repo:ministryofjustice/opg-reports:ref:refs/heads/*",
+  ]
+
+  custom_policy_documents = [data.aws_iam_policy_document.oidc_s3_access.json]
+
+}
+
+data "aws_iam_policy_document" "oidc_s3_access" {
+  version = "2012-10-17"
+
+  statement {
+    sid    = "OIDCS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      module.data_bucket.bucket.arn,
+      "${module.data_bucket.bucket.arn}/*",
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+}
