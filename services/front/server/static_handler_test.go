aff77201517ba632a98aa0e40f5d2341ee877092
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-08-01 11:08:21 +0100

    setup test to check all end points

--- services/front/server/static_handler_test.go
+++ services/front/server/static_handler_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	th "opg-reports/internal/testhelpers"
 	"opg-reports/services/front/cnf"
@@ -19,23 +20,37 @@ func TestFrontServerStaticHandler(t *testing.T) {
 	templates := tmpl.Files(f, tDir)
 
 	mux := th.Mux()
-	conf, _ := cnf.Load([]byte(testRealisticServerCnf))
+	conf, _ := cnf.Load(testRealServerCnf())
 	s := New(conf, templates, "", "")
 	s.Register(mux)
 
-	route := "/costs/"
-	w, r := th.WRGet(route)
-
-	mux.ServeHTTP(w, r)
-
-	if w.Result().StatusCode != http.StatusOK {
-		t.Errorf("should return 200")
+	// --- TEST KNOWN TOP LEVEL STATIC ROUTES
+	routes := []string{}
+	for _, sect := range conf.Sections {
+		if len(sect.Api) == 0 {
+			routes = append(routes, sect.Href)
+		}
+		for _, s := range sect.Sections {
+			if len(s.Api) == 0 {
+				routes = append(routes, s.Href)
+			}
+		}
 	}
 
-	str, _ := response.Stringify(w.Result())
+	for _, route := range routes {
+		w, r := th.WRGet(route)
+		mux.ServeHTTP(w, r)
 
-	if !strings.Contains(str, "OPG Report") {
-		t.Errorf("org not rendered")
+		if w.Result().StatusCode != http.StatusOK {
+			t.Errorf("should return 200: %v", w.Result().StatusCode)
+		}
+
+		str, _ := response.Stringify(w.Result())
+		if !strings.Contains(str, "OPG") {
+			fmt.Println(str)
+			t.Errorf("org not rendered for route [%s]", route)
+
+		}
 	}
 
 }
