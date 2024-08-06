92e5b6919fb94f13c4c687d10909665438f9b7c5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-08-06 16:29:33 +0100

    update test to use new endpoints etc

--- services/front/server/dynamic_handlers_test.go
+++ services/front/server/dynamic_handlers_test.go
@@ -1,39 +1,61 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
-	th "opg-reports/internal/testhelpers"
+	"opg-reports/internal/testhelpers"
+	"opg-reports/services/api/aws/cost/monthly"
 	"opg-reports/services/front/cnf"
 	"opg-reports/services/front/tmpl"
+	"opg-reports/shared/aws/cost"
+	"opg-reports/shared/data"
+	"opg-reports/shared/dates"
+	"opg-reports/shared/fake"
 	"opg-reports/shared/files"
 	"opg-reports/shared/logger"
-	"opg-reports/shared/server/response"
+	"opg-reports/shared/server/resp"
 	"os"
 	"strings"
 	"testing"
-	"time"
 )
 
-type testEntry struct {
-	Id       string `json:"id"`
-	Tag      string `json:"tag"`
-	Category string `json:"category"`
-}
-
-func (i *testEntry) UID() string {
-	return i.Id
-}
-func (i *testEntry) TS() time.Time {
-	return time.Now().UTC()
-}
-func (i *testEntry) Valid() bool {
-	return true
-}
-
-func TestFrontServerDynamicHandlerMockedTotals(t *testing.T) {
+func TestFrontServerDynamicHandlerTestAwsMonthlyTotalsTemplate(t *testing.T) {
 	logger.LogSetup()
+	// dates
+	min, max, df := testhelpers.Dates()
+	// -- get a faked api response
+	// items
+	count := 10
+	store := data.NewStore[*cost.Cost]()
+	services := []string{"ec2", "ecs", "tax", "rds", "r53"}
+	for i := 0; i < count; i++ {
+		c := cost.Fake(nil, min, max, df)
+		c.Service = fake.Choice(services)
+		store.Add(c)
+	}
+	// params
+	allow := []string{"version", "start", "end"}
+	params := map[string][]string{
+		"start": {min.Format(dates.FormatYM)},
+		"end":   {max.Format(dates.FormatYM)},
+	}
+	// functions
+	var headerF = monthly.DisplayHeadFunctions(params)["monthlyTotals"]
+	var rowF = monthly.DisplayRowFunctions(params)["monthlyTotals"]
+	// setup route
+	apiroute := fmt.Sprintf("/aws/costs/v1/monthly/%s/%s/",
+		min.Format(dates.FormatYM),
+		max.Format(dates.FormatYM))
+	//
+	w, r := testhelpers.WRGet(apiroute)
+	r.SetPathValue("start", min.Format(dates.FormatYM))
+	r.SetPathValue("end", max.Format(dates.FormatYM))
+	ep := testhelpers.MockEndpoint[*cost.Cost](store, allow, headerF, rowF, w, r)
+	ep.ProcessRequest(w, r)
+	apiresponse, _ := resp.Stringify(w.Result())
 
-	ms := th.MockServer(mockAwsCostMonthlyTotals(), http.StatusOK)
+	// -- test the front end response
+	ms := testhelpers.MockServer(apiresponse, http.StatusOK)
 	defer ms.Close()
 
 	tDir := "../templates/"
@@ -48,53 +70,21 @@ func TestFrontServerDynamicHandlerMockedTotals(t *testing.T) {
 	// point the totals route to look at the test api
 	s.Nav.Get(route).Api = map[string]string{"url": ms.URL}
 	//
-	mux := th.Mux()
+	mux := testhelpers.Mux()
 	s.Register(mux)
 	// now we fetch the local route, which should them call the mocked
 	// server
-	w, r := th.WRGet(route)
+	w, r = testhelpers.WRGet(route)
 	mux.ServeHTTP(w, r)
 
 	if w.Result().StatusCode != http.StatusOK {
 		t.Errorf("unexpected error")
 	}
 
-	str, _ := response.Stringify(w.Result())
+	str, _ := resp.Stringify(w.Result())
 	if !strings.Contains(str, "AWS Costs") {
+		fmt.Println(str)
 		t.Errorf("failed, costs header not found")
 	}
-	// fmt.Println(str)
 
-}
-
-func TestFrontServerDynamicHandlerMockedUnits(t *testing.T) {
-	logger.LogSetup()
-	content := mockAwsCostMonthlyUnits()
-	ms := th.MockServer(content, http.StatusOK)
-	defer ms.Close()
-
-	tDir := "../templates/"
-	dfSys := os.DirFS(tDir).(files.IReadFS)
-	f := files.NewFS(dfSys, tDir)
-	templates := tmpl.Files(f, tDir)
-
-	route := "/costs/aws/units/"
-	conf, _ := cnf.Load(testRealServerCnf())
-	// create new
-	s := New(conf, templates, "", "")
-	// point the totals route to look at the test api
-	s.Nav.Get(route).Api = map[string]string{"url": ms.URL}
-	//
-	mux := th.Mux()
-	s.Register(mux)
-	// now we fetch the local route, which should them call the mocked
-	// server
-	w, r := th.WRGet(route)
-	mux.ServeHTTP(w, r)
-
-	str, _ := response.Stringify(w.Result())
-	if !strings.Contains(str, "Costs Per Unit") {
-		t.Errorf("failed, costs header not found")
-	}
-	// fmt.Println(str)
 }
