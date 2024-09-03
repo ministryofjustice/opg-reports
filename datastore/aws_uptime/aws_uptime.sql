ec0b4ceeca580313b2afeb3e1be480e96e5984f4
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-09-03 09:48:02 +0100

    sqlc setup for uptime

---
+++ datastore/aws_uptime/aws_uptime.sql
@@ -0,0 +1,21 @@
+--
+CREATE TABLE aws_uptime (
+    -- auto inc primary key
+    id INTEGER PRIMARY KEY,
+    -- timestamp for when we generated this record
+    ts TEXT NOT NULL,
+    --
+    unit TEXT NOT NULL,
+    average INTEGER NOT NULL,
+    date TEXT NOT NULL
+    --
+) STRICT;
+
+-- used to track the dates of when the data was pulled
+CREATE TABLE aws_uptime_tracker (
+    id INTEGER PRIMARY KEY,
+    run_date TEXT NOT NULL
+) STRICT ;
+
+--
+CREATE INDEX aws_uptime_date_idx ON aws_costs(date);
