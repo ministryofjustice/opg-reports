830b259f91691ef1613c0a9c4360beda36120b7d
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-10-30 13:09:45 +0000

    add helper method

--- pkg/bi/bi.go
+++ pkg/bi/bi.go
@@ -2,8 +2,20 @@
 // during build that change values within the applications
 package bi
 
+import "log/slog"
+
 var Semver string = "v0.0.1"
 var Commit string = "0"
 var Timestamp string = "2024-10-01T01:02:03Z00:00"
 var Organisation string = "OPG"
 var ApiVersion string = "v1"
+
+func Dump() {
+	slog.Info("Build info",
+		slog.String("ApiVersion", ApiVersion),
+		slog.String("Commit", Commit),
+		slog.String("Organisation", Organisation),
+		slog.String("Semver", Semver),
+		slog.String("Timestamp", Timestamp),
+	)
+}
