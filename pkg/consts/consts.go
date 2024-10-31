874229d9c80258ab16c318d041a6b49971e18098
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-10-31 18:43:35 +0000

    update version

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -25,7 +25,7 @@ const (
 const CostsBillingDay int = 15
 
 // GovUKFrontendVersion is the current release being used for css etc
-const GovUKFrontendVersion string = "5.4.1"
+const GovUKFrontendVersion string = "5.7.1"
 
 const (
 	ServerDefaultFrontAddr string = "localhost:8080"
