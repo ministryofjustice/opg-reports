0433ab202bc64e7f2c951da1b1ab8fc70eb1c71d
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-06-26 13:02:56 +0100

    taz ball creation and tests

--- report/internal/utils/tar.go
+++ report/internal/utils/tar.go
@@ -3,21 +3,104 @@ package utils
 import (
 	"archive/tar"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
 
+func addToArchive(tw *tar.Writer, filename string) error {
+	// Open the file which will be written into the archive
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Get FileInfo about our file providing file size, mode, etc.
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	// Create a tar Header from the FileInfo data
+	header, err := tar.FileInfoHeader(info, info.Name())
+	if err != nil {
+		return err
+	}
+
+	// Use full path as name (FileInfoHeader only takes the basename)
+	// If we don't do this the directory strucuture would
+	// not be preserved
+	// https://golang.org/src/archive/tar/common.go?#L626
+	header.Name = filename
+
+	// Write file header to the tar archive
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+
+	// Copy file content to tar archive
+	_, err = io.Copy(tw, file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TarGzCreate takes a slice of filenames and generates a
+// tar.gz archive from those at the location specificed by
+// writeTo.
+//
+// If the writeTo location exists, or any of the files cannot
+// be found/opened then an error is returned.
+func TarGzCreate(writeTo string, files []string) (err error) {
+	var (
+		fp    *os.File
+		gzipw *gzip.Writer
+		tarw  *tar.Writer
+
+		parentDir string = filepath.Dir(writeTo)
+	)
+	// if destination exists, return error so we dont overwrite
+	if FileExists(writeTo) {
+		return fmt.Errorf("file [%s] already exists, wont overwrite", writeTo)
+	}
+	// create the parent directory path
+	err = os.MkdirAll(parentDir, os.ModePerm)
+	// create the file
+	fp, err = os.Create(writeTo)
+	if err != nil {
+		return
+	}
+	// now create the archives & close them via defer
+	gzipw = gzip.NewWriter(fp)
+	tarw = tar.NewWriter(gzipw)
+	defer gzipw.Close()
+	defer tarw.Close()
+
+	for _, file := range files {
+		err = addToArchive(tarw, file)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
 // TarGzExtract extract the tar.gz file passed into the directory passed.
 //
 // The extractTo path is appended to the filepaths reported by the tar.gz
 // structure to allow extraction to a fixed location
-func TarGzExtract(targz io.Reader, extractTo string) (err error) {
+func TarGzExtract(extractTo string, source io.Reader) (err error) {
 	var (
 		uncompressed *gzip.Reader
 		tarReader    *tar.Reader
 	)
-	uncompressed, err = gzip.NewReader(targz)
+	uncompressed, err = gzip.NewReader(source)
 	if err != nil {
 		return
 	}
@@ -31,6 +114,7 @@ func TarGzExtract(targz io.Reader, extractTo string) (err error) {
 
 		// grab next & handle errors
 		header, err = tarReader.Next()
+
 		// end of tar ball - not actually an error
 		if err == io.EOF {
 			err = nil
@@ -51,6 +135,11 @@ func TarGzExtract(targz io.Reader, extractTo string) (err error) {
 		// files
 		case tar.TypeReg:
 			var outFile *os.File
+			// create parent directory as well in case we get to a
+			// file before a folder
+			parent := filepath.Dir(header.Name)
+			os.MkdirAll(parent, os.ModePerm)
+
 			// create file
 			outFile, err = os.Create(header.Name)
 			if err != nil {
---
+++ report/internal/utils/taz_test.go
@@ -0,0 +1,81 @@
+package utils_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ministryofjustice/opg-reports/report/internal/utils"
+)
+
+type testTar struct {
+	Name string `json:"name"`
+}
+
+func TestTarGzSuccess(t *testing.T) {
+	var (
+		err         error
+		dir         = "./" //t.TempDir()
+		srcDir      = filepath.Join(dir, "src")
+		srcFileA    = filepath.Join(srcDir, "a.json")
+		srcFileB    = filepath.Join(srcDir, "b.json")
+		srcDataA    = []*testTar{{Name: "A"}, {Name: "B"}}
+		srcDataB    = []*testTar{{Name: "A"}, {Name: "B"}, {Name: "C"}}
+		destArchive = filepath.Join(dir, "archive", "archive.tar.gz")
+		extractTo   = filepath.Join(dir, "extract")
+	)
+
+	// generate a dummy files with some json in
+	utils.StructToJsonFile(srcFileA, srcDataA)
+	utils.StructToJsonFile(srcFileB, srcDataB)
+
+	// now compress those files into a tar ball
+	err = utils.TarGzCreate(destArchive, []string{srcFileA, srcFileB})
+	if err != nil {
+		t.Errorf("unexpected error compressing files: %s", err.Error())
+	}
+	if !utils.FileExists(destArchive) {
+		t.Errorf("archive file was not created in expected location")
+	}
+	// open archive
+	f, err := os.Open(destArchive)
+	if err != nil {
+		t.Errorf("unexpected error opening archive: %s", err.Error())
+	}
+	defer f.Close()
+	// now extract to a new folder
+	err = utils.TarGzExtract(extractTo, f)
+	if err != nil {
+		t.Errorf("unexpected error extracting files: %s", err.Error())
+	}
+
+	// check files extracted in know places
+	extractA := filepath.Join(dir, "extract", srcFileA)
+	extractB := filepath.Join(dir, "extract", srcFileB)
+	if !utils.FileExists(extractA) {
+		t.Errorf("could not find extracted file: %s", extractA)
+	}
+	if !utils.FileExists(extractB) {
+		t.Errorf("could not find extracted file: %s", extractB)
+	}
+
+	// now load up files and compare to original content to make sure
+	// number of structs match
+	structA := []*testTar{}
+	err = utils.StructFromJsonFile(extractA, &structA)
+	if err != nil {
+		t.Errorf("unexpected error compressing files: %s", err.Error())
+	}
+	if len(structA) != len(srcDataA) {
+		t.Errorf("number of records in extracted file did not match")
+	}
+	structB := []*testTar{}
+	err = utils.StructFromJsonFile(extractB, &structB)
+	if err != nil {
+		t.Errorf("unexpected error compressing files: %s", err.Error())
+	}
+	if len(structB) != len(srcDataB) {
+		t.Errorf("number of records in extracted file did not match")
+	}
+
+}
