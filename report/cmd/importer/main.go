f4057b1b20b01c0a0f3d24164d222b4233d19642
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-06-26 17:04:43 +0100

    tidy the import command

--- report/cmd/importer/main.go
+++ report/cmd/importer/main.go
@@ -12,96 +12,104 @@ import (
 	"github.com/ministryofjustice/opg-reports/report/config"
 	"github.com/ministryofjustice/opg-reports/report/internal/awsaccount"
 	"github.com/ministryofjustice/opg-reports/report/internal/awscost"
+	"github.com/ministryofjustice/opg-reports/report/internal/sqldb"
 	"github.com/ministryofjustice/opg-reports/report/internal/team"
 	"github.com/ministryofjustice/opg-reports/report/internal/utils"
 	"github.com/spf13/cobra"
 )
 
-// Get the configuration data and the viper config for mapping to cli args
-var conf, viperConf = config.New()
+var (
+	err             error
+	conf, viperConf                 = config.New() // Get the configuration data and the viper config for mapping to cli args
+	ctx             context.Context = context.Background()
+	log             *slog.Logger    = utils.Logger(conf.Log.Level, conf.Log.Type)
+)
 
-// useFixtures is flag set by cli args to decide if using fixture data
-var useFixtures bool = false
+var (
+	s3Bucket string = "" // s3Bucket tracks the --s3-bucket arg that determines if some data is fetched from existing json or from api
+)
+
+type seedFunc func(ctx context.Context, log *slog.Logger, conf *config.Config, seeds []*sqldb.BoundStatement) (inserted []*sqldb.BoundStatement, err error)
+type importFunc func(ctx context.Context, log *slog.Logger, conf *config.Config) (err error)
 
 // root command
 var rootCmd = &cobra.Command{
-	Use:   "importer",
-	Short: "Importer",
-	Long:  `importer imports everything.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			err error
-			ctx context.Context = context.Background()
-			log *slog.Logger    = utils.Logger(conf.Log.Level, conf.Log.Type)
-		)
+	Use:               "import",
+	Short:             "Import",
+	Long:              `import can populate database with fixture data ("fixtures"), fetch data from pre-existing json ("existing") or new data via specific external api's.`,
+	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+}
 
-		if useFixtures {
-			log.Info("using seed data ...")
-			err = seedData(ctx, log, conf)
-		} else {
-			log.Info("importing real data ...")
-			err = realData(ctx, log, conf)
-		}
-		if err != nil {
-			log.Error("error running imports", "error", err.Error())
-			return
-		}
+var existingCmd = &cobra.Command{
+	Use:   "existing",
+	Short: "existing imports all known existing data files.",
+	Long:  `existing imports all known data files (generally json) from a mix of sources (github, s3 buckets) that covers current and prior reporting data to ensure completeness`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 
+		err = realData(ctx, log, conf,
+			team.Import,
+			awsaccount.Import)
+
+		return
 	},
 }
 
-func seedData(ctx context.Context, log *slog.Logger, conf *config.Config) (err error) {
-	// Seed team data
-	_, err = team.Seed(ctx, log, conf, nil)
-	if err != nil {
+var fixturesCmd = &cobra.Command{
+	Use:   "fixtures",
+	Short: "fixtures uses known data to populate the database",
+	Long:  `fixtures empties and then populates the database with a series of known data sets to allow a create test instance.`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+		err = seedData(ctx, log, conf,
+			team.Seed,
+			awsaccount.Seed,
+			awscost.Seed)
 		return
+	},
+}
+
+// seedData runs seed calls
+func seedData(ctx context.Context, log *slog.Logger, conf *config.Config, seeds ...seedFunc) (err error) {
+
+	log.Info("using seed data ...")
+	for _, lambda := range seeds {
+		_, err = lambda(ctx, log, conf, nil)
+		if err != nil {
+			return
+		}
 	}
-	// Seed awsaccounts
-	_, err = awsaccount.Seed(ctx, log, conf, nil)
-	if err != nil {
-		return
-	}
-	// Seed awscosts
-	_, err = awscost.Seed(ctx, log, conf, nil)
-	if err != nil {
-		return
-	}
+
 	return
 }
 
 // Run the importers that will use real data
-func realData(ctx context.Context, log *slog.Logger, conf *config.Config) (err error) {
-	// Import teams
-	err = team.Import(ctx, log, conf)
-	if err != nil {
-		return
+func realData(ctx context.Context, log *slog.Logger, conf *config.Config, imports ...importFunc) (err error) {
+
+	log.Info("using real data ...")
+	for _, lambda := range imports {
+		err = lambda(ctx, log, conf)
+		if err != nil {
+			return
+		}
 	}
-	// Import accounts
-	err = awsaccount.Import(ctx, log, conf)
-	if err != nil {
-		return
-	}
+
 	return
 }
 
 // init
-// - Binds global config values to parameters
 func init() {
+	// Global flags for all commands:
 	// bind the database.path config item
 	rootCmd.PersistentFlags().StringVar(&conf.Database.Path, "database.path", conf.Database.Path, "Path to database file")
 	viperConf.BindPFlag("database.path", rootCmd.PersistentFlags().Lookup("database.path"))
-
-	// bind the github.organisation config item to the shorter --org
+	// bind the github.organisation for those commands that require it
 	rootCmd.PersistentFlags().StringVar(&conf.Github.Organisation, "github.organisation", conf.Github.Organisation, "GitHub organisation name")
 	viperConf.BindPFlag("github.organisation", rootCmd.PersistentFlags().Lookup("github.organisation"))
-
-	// bind a flag to decide if we are using fixture data or not
-	rootCmd.PersistentFlags().BoolVar(&useFixtures, "fixtures", false, "When true, use a small predtermined dataset.")
 
 }
 
 func main() {
-
+	rootCmd.AddCommand(existingCmd, fixturesCmd)
 	rootCmd.Execute()
 
 }
