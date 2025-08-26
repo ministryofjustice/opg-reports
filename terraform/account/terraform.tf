2b05970525526209682350d4e6c03b25e6b9e8eb
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-08-26 08:26:56 +0100

    provider parameter change

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -1,12 +1,16 @@
 terraform {
 
   backend "s3" {
-    bucket         = "opg.terraform.state"
-    key            = "opg-reports-account/terraform.tfstate"
-    encrypt        = true
-    region         = "eu-west-1"
-    role_arn       = "arn:aws:iam::311462405659:role/opg-reporting-state-access"
-    dynamodb_table = "remote_lock"
+    bucket       = "opg.terraform.state"
+    key          = "opg-reports-account/terraform.tfstate"
+    encrypt      = true
+    region       = "eu-west-1"
+    use_lockfile = true
+
+    assume_role = {
+      role_arn = "arn:aws:iam::311462405659:role/opg-reporting-state-access"
+    }
+
   }
 
 }
