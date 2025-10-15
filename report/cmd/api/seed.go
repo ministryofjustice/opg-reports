8d940601d47f68b3976d1efb9e87c1ade2093cef
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-10-15 16:40:49 +0100

    api no longer seeds the db

--- report/cmd/api/seed.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// // seedDB is called if the database doesnt exist on init, so creates a dummy one
-// func SeedDB(ctx context.Context, log *slog.Logger, conf *config.Config) (err error) {
-// 	var sqlStore sqlr.RepositoryWriter = sqlr.Default(ctx, log, conf)
-// 	var seedService *seed.Service = seed.Default(ctx, log, conf)
-// 	_, err = seedService.All(sqlStore)
-// 	return
-// }
