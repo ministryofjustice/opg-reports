8bbfd80a8ef8494f5115cebe0756da8bf46a0bd5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-12-04 10:05:07 +0000

    migrations for simple codeownership table

--- report/internal/repository/sqlr/schema.go
+++ report/internal/repository/sqlr/schema.go
@@ -1,11 +1,13 @@
 package sqlr
 
+// -- TEAMS
 const migration_teams_table string = `
 CREATE TABLE IF NOT EXISTS teams (
 	name TEXT PRIMARY KEY,
 	created_at TEXT NOT NULL DEFAULT (strftime('%FT%TZ', 'now') )
 ) STRICT;`
 
+// -- AWS ACCOUNTS
 const migration_aws_accounts_table string = `
 CREATE TABLE IF NOT EXISTS aws_accounts (
 	id TEXT PRIMARY KEY,
@@ -18,6 +20,7 @@ CREATE TABLE IF NOT EXISTS aws_accounts (
 ) WITHOUT ROWID;`
 const migration_aws_account_idx string = `CREATE INDEX IF NOT EXISTS aws_accounts_id_idx ON aws_accounts(id);`
 
+// -- AWS COSTS
 const migration_aws_costs_table string = `
 CREATE TABLE IF NOT EXISTS aws_costs (
 	id INTEGER PRIMARY KEY,
@@ -33,6 +36,7 @@ const migration_aws_costs_idx_date string = `CREATE INDEX IF NOT EXISTS aws_cost
 const migration_aws_costs_idx_date_account string = `CREATE INDEX IF NOT EXISTS aws_costs_date_account_idx ON aws_costs(date, aws_account_id);`
 const migration_aws_costs_idx_merged string = `CREATE INDEX IF NOT EXISTS aws_costs_unique_idx ON aws_costs(aws_account_id,date,region,service);`
 
+// -- AWS UPTIME
 const migration_aws_uptime_table string = `
 CREATE TABLE IF NOT EXISTS aws_uptime (
 	id INTEGER PRIMARY KEY,
@@ -42,20 +46,44 @@ CREATE TABLE IF NOT EXISTS aws_uptime (
 	average TEXT NOT NULL,
 	granularity TEXT NOT NULL,
 	UNIQUE (aws_account_id,date)
-) STRICT;
-`
+) STRICT;`
 const migration_aws_uptime_idx_date string = `CREATE INDEX IF NOT EXISTS aws_uptime_date_idx ON aws_uptime(date);`
 const migration_aws_uptime_idx_date_account string = `CREATE INDEX IF NOT EXISTS aws_uptime_account_date_idx ON aws_uptime(aws_account_id,date);`
 
+// -- GITHUB CODEOWNERS
+const migration_github_codeowner_table string = `
+CREATE TABLE IF NOT EXISTS github_codeownership (
+	id INTEGER PRIMARY KEY,
+	created_at TEXT NOT NULL DEFAULT (strftime('%FT%TZ', 'now') ),
+	codeowner TEXT NOT NULL,
+	repository TEXT NOT NULL,
+	team TEXT NOT NULL,
+	UNIQUE (codeowner,repository,team)
+) STRICT;` // repository & team will be a link to other tables as text primary keys
+const migration_github_codeowner_all_idx string = `CREATE INDEX IF NOT EXISTS gh_codeownership_all_idx ON github_codeownership(codeowner,repository,team);`
+const migration_github_codeowner_idx string = `CREATE INDEX IF NOT EXISTS gh_codeownership_codeowner_idx ON github_codeownership(codeowner);`
+const migration_github_repo_idx string = `CREATE INDEX IF NOT EXISTS gh_codeownership_repository ON github_codeownership(repository);`
+const migration_github_team_idx string = `CREATE INDEX IF NOT EXISTS gh_codeownership_team ON github_codeownership(team);`
+
 var DB_MIGRATIONS_UP []string = []string{
+	// base teams
 	migration_teams_table,
+	// aws accounts
 	migration_aws_accounts_table,
 	migration_aws_account_idx,
+	// aws costs
 	migration_aws_costs_table,
 	migration_aws_costs_idx_date,
 	migration_aws_costs_idx_date_account,
 	migration_aws_costs_idx_merged,
+	// aws uptime
 	migration_aws_uptime_table,
 	migration_aws_uptime_idx_date,
 	migration_aws_uptime_idx_date_account,
+	// github code ownership
+	migration_github_codeowner_table,
+	migration_github_codeowner_all_idx,
+	migration_github_codeowner_idx,
+	migration_github_repo_idx,
+	migration_github_team_idx,
 }
