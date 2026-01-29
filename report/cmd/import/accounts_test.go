fc185ea87a17c1ee3bf44f08cd01c1233438d2cc
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2026-01-29 14:05:58 +0000

    mock account import

--- report/cmd/import/accounts_test.go
+++ report/cmd/import/accounts_test.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"log/slog"
+	"net/http"
 	"opg-reports/report/internal/db/dbconnection"
 	"opg-reports/report/internal/db/dbmigrations"
+	"opg-reports/report/internal/domain/accounts/accountmodels"
+	"opg-reports/report/internal/domain/accounts/accountselects"
 	"opg-reports/report/internal/utils/ghclients"
 	"opg-reports/report/internal/utils/logger"
+	"opg-reports/report/internal/utils/marshal"
+	"opg-reports/report/internal/utils/ptr"
+	"opg-reports/report/internal/utils/zips"
 	"os"
 	"path/filepath"
 	"testing"
@@ -14,6 +22,84 @@ import (
 	"github.com/google/go-github/v81/github"
 	"github.com/jmoiron/sqlx"
 )
+
+// mockClient is a mocked client that returns successful results
+type mockClient struct{}
+
+// GetReleaseByTag mocked version to return preset version
+func (self *mockClient) GetReleaseByTag(ctx context.Context, owner, repo, tag string) (rel *github.RepositoryRelease, resp *github.Response, err error) {
+	var (
+		mockTar = &github.ReleaseAsset{
+			ID:          ptr.Ptr[int64](20001),
+			Name:        ptr.Ptr("metadata.tar.gz"),
+			ContentType: ptr.Ptr("application/tar+gzip"),
+		}
+		mockZip = &github.ReleaseAsset{
+			ID:          ptr.Ptr[int64](20002),
+			Name:        ptr.Ptr("metadata.zip"),
+			ContentType: ptr.Ptr("application/zip"),
+		}
+	)
+	resp = &github.Response{
+		NextPage: 0,
+		Response: &http.Response{
+			StatusCode: http.StatusOK,
+		},
+	}
+	rel = &github.RepositoryRelease{
+		Name:       ptr.Ptr("mock-release"),
+		Draft:      ptr.Ptr(false),
+		Prerelease: ptr.Ptr(false),
+		TagName:    ptr.Ptr(tag),
+		Assets: []*github.ReleaseAsset{
+			mockTar,
+			mockZip,
+		},
+	}
+	return
+}
+
+// DownloadReleaseAsset returns mocked version with the asset name as the content of the file
+func (self *mockClient) DownloadReleaseAsset(ctx context.Context, owner, repo string, id int64, followRedirectsClient *http.Client) (rc io.ReadCloser, redirectURL string, err error) {
+
+	// create a temp zip file with account.aws.json file which contains dummy data
+	// - write to tmp location, read and stream
+	content := createDummyZip()
+	rc = io.NopCloser(bytes.NewBuffer(content))
+	return
+}
+
+func TestImportsAccountsWithMock(t *testing.T) {
+	var (
+		err    error
+		db     *sqlx.DB
+		client *mockClient     = &mockClient{}
+		ctx    context.Context = t.Context()
+		log    *slog.Logger    = logger.New("error")
+		dir    string          = t.TempDir()
+		dbPath string          = filepath.Join(dir, "test-import-mock-accounts.db")
+	)
+	// set the database
+	db, err = dbconnection.Connection(ctx, log, "sqlite3", dbPath)
+	if err != nil {
+		t.Errorf("unexpected connection error: [%s]", err.Error())
+		t.FailNow()
+	}
+	dbmigrations.Migrate(ctx, log, db)
+	defer db.Close()
+
+	err = importAccounts(ctx, log, client, db)
+	if err != nil {
+		t.Errorf("unexpected import error: [%s]", err.Error())
+		t.FailNow()
+	}
+
+	res, err := accountselects.All(ctx, log, db)
+	if len(res) != 2 {
+		t.Errorf("expected exactly 2 accounts from the mock data to be created.")
+	}
+
+}
 
 func TestImportsAccountsWithoutMock(t *testing.T) {
 
@@ -47,4 +133,47 @@ func TestImportsAccountsWithoutMock(t *testing.T) {
 		t.SkipNow()
 	}
 
+}
+
+// createDummyZip makes fake accounts json file, then builds a zip
+// and from that reads the zip and returns the content to simulate
+// a downloaded file
+func createDummyZip() []byte {
+	var mockAccounts = []*accountmodels.AwsAccount{
+		{
+			ID:          "mock-account-01A",
+			Name:        "mock-account-dev",
+			Label:       "mock-dev",
+			Environment: "development",
+			TeamName:    "mock-team",
+		},
+		{
+			ID:          "mock-account-01B",
+			Name:        "mock-account-prod",
+			Label:       "mock-prod",
+			Environment: "prod",
+			TeamName:    "mock-team",
+		},
+	}
+
+	tmpDir, _ := os.MkdirTemp("", "mock-account-*")
+	// tmpDir := "./tmp"
+	dataDir := filepath.Join(tmpDir, "src")
+	os.MkdirAll(dataDir, os.ModePerm)
+	// write the accounts to dummy file
+	accountStr := marshal.ToString(mockAccounts)
+	file := filepath.Join(dataDir, "accounts.aws.json")
+	os.WriteFile(file, []byte(accountStr), os.ModePerm)
+
+	// create the zip
+	zipFile := filepath.Join(tmpDir, "metadata.zip")
+	zips.Create(zipFile, []string{file}, dataDir)
+	// read content and return
+	content, _ := os.ReadFile(zipFile)
+	// remove the tmp dir and its content
+	defer func() {
+		os.RemoveAll(tmpDir)
+	}()
+
+	return content
 }
---
+++ report/internal/domain/accounts/accountselects/selects.go
@@ -0,0 +1,52 @@
+package accountselects
+
+import (
+	"context"
+	"log/slog"
+	"opg-reports/report/internal/db/dbselects"
+	"opg-reports/report/internal/db/dbstatements"
+	"opg-reports/report/internal/domain/accounts/accountmodels"
+
+	"github.com/jmoiron/sqlx"
+)
+
+const selectAllStmt string = `
+SELECT
+	id,
+	name,
+	label,
+	environment,
+	team_name
+FROM accounts
+ORDER BY name, environment ASC
+`
+
+// empty is used when no filtering / substitution on the sql statement, such
+// as a `select *` or `select count(*)`
+type empty struct{}
+
+func All(ctx context.Context, log *slog.Logger, db *sqlx.DB) (data []*accountmodels.AwsAccount, err error) {
+	var (
+		lg       *slog.Logger = log.With("func", "domain.accounts.accountselects.All")
+		selector *dbstatements.SelectStatement[*empty, *accountmodels.AwsAccount]
+	)
+	data = []*accountmodels.AwsAccount{}
+	// setup the select
+	selector = &dbstatements.SelectStatement[*empty, *accountmodels.AwsAccount]{
+		Statement: selectAllStmt,
+		Data:      &empty{},
+	}
+
+	lg.Debug("starting ...")
+	err = dbselects.Select(ctx, log, db, selector)
+	if err != nil {
+		lg.Error("error with select.", "err", err.Error())
+		return
+	}
+	// setup output
+	for _, row := range selector.Returned {
+		data = append(data, row)
+	}
+	lg.Debug("complete.")
+	return
+}
