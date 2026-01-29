ec613a58af98b02f33a8baade66d0a1b381e9493
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2026-01-29 13:41:25 +0000

    mocking for these functions

--- report/internal/domain/codebases/codebase/codebase_test.go
+++ report/internal/domain/codebases/codebase/codebase_test.go
@@ -3,14 +3,56 @@ package codebase
 import (
 	"context"
 	"log/slog"
+	"net/http"
 	"opg-reports/report/internal/domain/codebases/codebasemodels"
 	"opg-reports/report/internal/utils/ghclients"
 	"opg-reports/report/internal/utils/logger"
+	"opg-reports/report/internal/utils/ptr"
 	"os"
 	"testing"
 
 	"github.com/google/go-github/v81/github"
 )
+
+// mockGetter is a mocked client that returns successful results
+type mockGetter struct{}
+
+func (self *mockGetter) ListTeamReposBySlug(ctx context.Context, org, slug string, opts *github.ListOptions) (repos []*github.Repository, resp *github.Response, err error) {
+
+	repos = []*github.Repository{
+		{Name: ptr.Ptr("mock-repo-a"), FullName: ptr.Ptr("mock/mock-repo-a"), HTMLURL: ptr.Ptr("https://test.local/mock/mock-repo-a"), Archived: ptr.Ptr(false)},
+		{Name: ptr.Ptr("mock-repo-b"), FullName: ptr.Ptr("mock/mock-repo-b"), HTMLURL: ptr.Ptr("https://test.local/mock/mock-repo-b"), Archived: ptr.Ptr(false)},
+		{Name: ptr.Ptr("mock-repo-c"), FullName: ptr.Ptr("mock/mock-repo-c"), HTMLURL: ptr.Ptr("https://test.local/mock/mock-repo-c"), Archived: ptr.Ptr(true)},
+	}
+	resp = &github.Response{
+		NextPage: 0,
+		Response: &http.Response{
+			StatusCode: http.StatusOK,
+		},
+	}
+	return
+}
+
+func TestDomainCodebasesWithMock(t *testing.T) {
+
+	var (
+		err    error
+		client *mockGetter     = &mockGetter{}
+		ctx    context.Context = t.Context()
+		log    *slog.Logger    = logger.New("error")
+		opts   *Options        = &Options{ExcludeArchived: true}
+		data   []*codebasemodels.Codebase
+	)
+
+	data, err = GetCodebases(ctx, log, client, opts)
+	if err != nil {
+		t.Errorf("unexpected error:\n%s", err.Error())
+	}
+	if len(data) != 2 {
+		t.Errorf("expected exactly two teams in the list.")
+	}
+
+}
 
 func TestDomainCodebasesWithoutMock(t *testing.T) {
 
@@ -22,8 +64,8 @@ func TestDomainCodebasesWithoutMock(t *testing.T) {
 		data   []*codebasemodels.Codebase
 	)
 
-	if os.Getenv("GITHUB_TOKEN") != "" {
-		client, err = ghclients.New(ctx, log, os.Getenv("GITHUB_TOKEN"))
+	if os.Getenv("GH_TOKEN") != "" {
+		client, err = ghclients.New(ctx, log, os.Getenv("GH_TOKEN"))
 		if err != nil {
 			t.Errorf("unexpected error:\n%s", err.Error())
 		}
--- report/internal/domain/codeowners/codeowner/codeowner_test.go
+++ report/internal/domain/codeowners/codeowner/codeowner_test.go
@@ -1,17 +1,137 @@
 package codeowner
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
 	"opg-reports/report/internal/domain/codebases/codebasemodels"
 	"opg-reports/report/internal/domain/codeowners/codeownermodels"
+	"opg-reports/report/internal/utils/debugger"
 	"opg-reports/report/internal/utils/ghclients"
 	"opg-reports/report/internal/utils/logger"
+	"opg-reports/report/internal/utils/ptr"
 	"os"
 	"testing"
 
 	"github.com/google/go-github/v81/github"
 )
+
+type mockGetter struct{}
+
+func (self *mockGetter) ListTeams(ctx context.Context, owner, repo string, opts *github.ListOptions) (teams []*github.Team, resp *github.Response, err error) {
+	parent := &github.Team{
+		Slug: ptr.Ptr(requiredTeamParent),
+	}
+
+	teams = []*github.Team{
+		{
+			Name:    ptr.Ptr("mock-other"),
+			Slug:    ptr.Ptr("mock-other"),
+			HTMLURL: ptr.Ptr("https://github.com/orgs/" + owner + "/mock-other"),
+		},
+		{
+			Name:    ptr.Ptr("mock-team-a"),
+			Slug:    ptr.Ptr("mock-team-a"),
+			HTMLURL: ptr.Ptr("https://github.com/orgs/" + owner + "/mock-team-a"),
+			Parent:  parent,
+		},
+		{
+			Name:    ptr.Ptr("mock-team-b"),
+			Slug:    ptr.Ptr("mock-team-b"),
+			HTMLURL: ptr.Ptr("https://github.com/orgs/" + owner + "/mock-team-b"),
+			Parent:  parent,
+		},
+	}
+	// add extra team for mock-repo-b
+	if repo == "mock-repo-b" {
+		teams = append(teams, &github.Team{
+			Name:    ptr.Ptr("mock-team-c"),
+			Slug:    ptr.Ptr("mock-team-c"),
+			HTMLURL: ptr.Ptr("https://github.com/orgs/" + owner + "/mock-team-c"),
+			Parent:  parent,
+		})
+	}
+
+	resp = &github.Response{
+		NextPage: 0,
+		Response: &http.Response{
+			StatusCode: http.StatusOK,
+		},
+	}
+	return
+}
+
+func (self *mockGetter) DownloadContents(ctx context.Context, owner, repo, filepath string, opts *github.RepositoryContentGetOptions) (buff io.ReadCloser, resp *github.Response, err error) {
+
+	content := fmt.Sprintf(`* @%s/mock-team-a @%s/mock-team-b%s`, owner, owner, "\n")
+	if repo == "mock-repo-b" {
+		content += fmt.Sprintf(`.github/  @%s/mock-team-codeowner%s`, owner, "\n")
+	}
+	buff = io.NopCloser(bytes.NewBuffer([]byte(content)))
+
+	resp = &github.Response{
+		NextPage: 0,
+		Response: &http.Response{
+			StatusCode: http.StatusOK,
+		},
+	}
+	return
+}
+
+func TestDomainCodebasesWithMock(t *testing.T) {
+
+	var (
+		err    error
+		opts   *Input
+		client *mockGetter                  = &mockGetter{}
+		ctx    context.Context              = t.Context()
+		log    *slog.Logger                 = logger.New("error")
+		data   []*codeownermodels.Codeowner = []*codeownermodels.Codeowner{}
+	)
+
+	opts = &Input{
+		Codebases: []*codebasemodels.Codebase{
+			{FullName: githubOrg + "/mock-repo-a", Name: "mock-repo-a"},
+			{FullName: githubOrg + "/mock-repo-b", Name: "mock-repo-b"},
+			{FullName: githubOrg + "/mock-repo-c", Name: "mock-repo-c"},
+		},
+	}
+
+	data, err = GetCodeowners(ctx, log, client, opts)
+	if err != nil {
+		t.Errorf("unexpected error:\n%s", err.Error())
+	}
+	if len(data) < 1 {
+		t.Errorf("unexpected count of results")
+	}
+
+	foundOther := false // this team did not have correct parent..
+	for _, row := range data {
+		if row.Name == "mock-other" {
+			foundOther = true
+		}
+	}
+	if foundOther {
+		t.Errorf("returned data included a mock team that should have been excluded.")
+	}
+
+	foundExtraBTeam := false
+	for _, row := range data {
+		if row.Name == "mock-team-c" && row.CodebaseFullName == githubOrg+"/mock-repo-b" {
+			foundExtraBTeam = true
+		}
+	}
+	if foundExtraBTeam {
+		t.Errorf("returned data did not include extrea team for the b-repo.")
+	}
+
+	debugger.Dump(data)
+	t.FailNow()
+
+}
 
 func TestDomainCodeownerWithoutMock(t *testing.T) {
 
@@ -44,7 +164,6 @@ func TestDomainCodeownerWithoutMock(t *testing.T) {
 		if len(result) <= 0 {
 			t.Errorf("unexpected count of results")
 		}
-
 	} else {
 		t.SkipNow()
 	}
